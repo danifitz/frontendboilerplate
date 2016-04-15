ac543fae7a7472c263a8a931bdf39752665c30f1
Daniel Fitzgerald <danfitzgerald90@gmail.com>
2016-04-15 20:35:07 +0100

    fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,39 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-	context: path.join(__dirname, './client'),
+	context: path.join(__dirname, './src'),
 	entry: {
-		jsx: './index.js'
-		vendor: [
-			'react',
-			'react-dom'
-		]
+		jsx: './index.jsx',
+		html: './index.html'
 	},
 	output: {
-		path: path.join(__dirname, './lib'),
+		path: path.join(__dirname, './static'),
 		filename: 'bundle.js'
 	},
 	module: {
-		loaders: [{
-			test: /\.(js|jsx)$/,
-			exclude: /node_modules/,
-			loaders: [
-				'babel-loader'
-			]
-		}]
+		loaders: [
+		    {
+			    test: /\.(js|jsx)$/,
+			    exclude: /node_modules/,
+			    loader: 'babel-loader',
+			    query: {
+			    	presets: ['es2015', 'react']
+			    }
+		    },
+		    {
+			    test: /\.html$/,
+			    loader: 'file?name=[name].[ext]'
+		    },
+		    {
+			    test: /\.css$/,
+			    include: /src/,
+			    loader: 'style-loader!css-loader'
+		    }
+		]
 	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
-			}
-		})
-	],
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
 	devServer: {
 		contentBase: './src',
 		hot: true
